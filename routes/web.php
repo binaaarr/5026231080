8f0fc21a390d973951b86e74971317f3ca3750a4
binaaarr <binarfaisha@gmail.com>
2025-05-21 12:35:26 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +53,15 @@ Route::get('Tugas6', function () {
 Route::get('ETS', function () {
 	return redirect()->away('https://binaaarr.github.io/');
 });
+
+
+Route::get('dosen', [Link::class, 'index']);
+Route::get('pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+//Route Blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
